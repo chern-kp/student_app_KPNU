0eca30170516926eecb81c4e1c33462bbbe04e14
chern-kp <cherniavskyi.kp@gmail.com>
2023-11-12 19:44:52 +0200

    recordbookpage

--- lib/pages/record_book_page.dart
+++ lib/pages/record_book_page.dart
@@ -1,7 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class RecordBookPage extends StatelessWidget {
+import '../class/database_service.dart';
+import '../components/my_dropdownmenu_semeter.dart';
+
+class RecordBookPage extends StatefulWidget {
   const RecordBookPage({Key? key}) : super(key: key);
+
+  @override
+  State<RecordBookPage> createState() => _RecordBookPageState();
+}
+
+class _RecordBookPageState extends State<RecordBookPage> {
+  late Future<String> selectedSemester =
+      DatabaseService.getStudentField(user.email, 'Current Semester');
+
+  String? selectedSemesterPage;
+  late Future<List<String>> semesterList =
+      DatabaseService.getSemesterList(user.email);
+
+  final user = FirebaseAuth.instance.currentUser!;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedSemester.then((value) {
+      setState(() {
+        selectedSemesterPage = value;
+      });
+    });
+  }
+
+  void updateSelectedSemester(String selectedItem) {
+    setState(() {
+      selectedSemesterPage = selectedItem;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,8 +44,12 @@ class RecordBookPage extends StatelessWidget {
         title: Text('RecordBookPage'),
         //todo
       ),
-      body: Center(
-        child: Text('RecordBookPage'),
+      body: Column(
+        children: [
+          Center(
+              child: MyDropdownMenuSemester(
+                  onSelectedItemChanged: updateSelectedSemester))
+        ],
       ),
     );
   }
