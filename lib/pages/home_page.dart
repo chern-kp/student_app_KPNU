47b33db4e07f516ea9e6dc29fa603d0b61fae204
chern-kp <cherniavskyi.kp@gmail.com>
2023-12-15 21:16:25 +0200

    fixes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -50,58 +50,73 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        actions: [
-          IconButton(onPressed: signUserOut, icon: const Icon(Icons.logout))
-        ],
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Column(
-              children: [
-                const Text("Ви увійшли як:", style: TextStyle(fontSize: 16)),
-                Text(user.email!,
-                    style: TextStyle(fontSize: 18, color: Colors.blue[700])),
+    return FutureBuilder(
+      future: Future.wait([facultyList, selectedSemester]),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return CircularProgressIndicator();
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else {
+          return Scaffold(
+            backgroundColor: Colors.white,
+            appBar: AppBar(
+              actions: [
+                IconButton(
+                    onPressed: signUserOut, icon: const Icon(Icons.logout))
               ],
             ),
-            const SizedBox(height: 25),
-            const Text('Оберіть поточний семестр:',
-                style: TextStyle(fontSize: 16)),
-            const SizedBox(height: 5),
-            DropdownMenuUserSemester(
-                listOfData: facultyList,
-                chosenValueInDatabase: selectedSemester,
-                chosenField: 'Current Semester'),
-            const SizedBox(height: 75),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MyButton(
-                onTap: () => coursesSchedulePageButton(context),
-                text: 'Індивідуальний Навчальний План',
+            body: Center(
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      const Text("Ви увійшли як:",
+                          style: TextStyle(fontSize: 16)),
+                      Text(user.email!,
+                          style:
+                              TextStyle(fontSize: 18, color: Colors.blue[700])),
+                    ],
+                  ),
+                  const SizedBox(height: 25),
+                  const Text('Оберіть поточний семестр:',
+                      style: TextStyle(fontSize: 16)),
+                  const SizedBox(height: 5),
+                  DropdownMenuUserSemester(
+                    listOfData: facultyList,
+                    chosenValueInDatabase: selectedSemester,
+                    chosenField: 'Current Semester',
+                  ),
+                  const SizedBox(height: 75),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MyButton(
+                      onTap: () => coursesSchedulePageButton(context),
+                      text: 'Індивідуальний Навчальний План',
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MyButton(
+                      onTap: () => recordBookPageButton(context),
+                      text: 'Залікова Книжка Студента',
+                    ),
+                  ),
+                  const SizedBox(height: 25),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: MyButton(
+                      onTap: () => classSchedulePageButton(context),
+                      text: 'Графік Освітнього Процесу',
+                    ),
+                  ),
+                ],
               ),
             ),
-            const SizedBox(height: 25),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MyButton(
-                onTap: () => recordBookPageButton(context),
-                text: 'Залікова Книжка Студента',
-              ),
-            ),
-            const SizedBox(height: 25),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: MyButton(
-                onTap: () => classSchedulePageButton(context),
-                text: 'Графік Освітнього Процесу',
-              ),
-            ),
-          ],
-        ),
-      ),
+          );
+        }
+      },
     );
   }
 }
